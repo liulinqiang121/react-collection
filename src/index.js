9e1486a553d97ad861d2eaa496057cad0640cdbf
liulinqiang <liulinqiang@turingcat.com>
2018-07-02 21:16:59 +0800

    新建路由文件

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
-import Manage from './components/managePage'
-import {BrowserRouter as Router,Route,Link,NavLink,Prompt,Switch} from 'react-router-dom'
+import RouterList from './router/index' 
+//import {BrowserRouter as Router,Route,Link,NavLink,Prompt,Switch} from 'react-router-dom'
+console.log(RouterList)
+ReactDOM.render((<RouterList/>), document.getElementById('app'));  
 
-ReactDOM.render((  
-    <Router>  
-        <div>  
-            <Route exact path="/" component={App}/>  
-            <Route path="/manage" component={Manage}/>  
-        </div>  
-    </Router>  
-), document.getElementById('root'));  
-ReactDOM.render(<div><Router /></div>, document.getElementById('app'));
+// ReactDOM.render((  
+//     <Router>  
+//         <div>  
+//             <Route exact path="/login" component={App}/>  
+//             <Route path="/manage" component={Manage}/>  
+//         </div>  
+//     </Router>  
+// ), document.getElementById('app'));  
