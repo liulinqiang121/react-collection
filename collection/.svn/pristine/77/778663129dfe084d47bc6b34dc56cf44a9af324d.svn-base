798f2c88be9724845123c022331cb0780ea6466b
liulinqiang <liulinqiang@turingcat.com>
2018-09-04 15:09:45 +0800

    删除不必要的文件

--- collection/.svn/pristine/77/778663129dfe084d47bc6b34dc56cf44a9af324d.svn-base
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-      <p>抱歉，您无访问权限</p>
-  </div>
-</template>
--- collection/dist/static/js/54.8f9fc637b2b1e5b4e5a1.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([54],{"0A6f":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){this.$createElement;this._self._c;return this._m(0)},staticRenderFns:[function(){var e=this.$createElement,t=this._self._c||e;return t("div",[t("p",[this._v("抱歉，您无访问权限")])])}]},r=n("vSla")(null,l,!1,null,null,null);t.default=r.exports}});
